65601a2493190b367f96c8ef8c4ed3f4ddeef46e
kevinrrh <kevinricardorosales8@gmail.com>
2025-06-16 18:02:29 -0600

    Endpoint

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const express =require('express')
-const app = express
-const PORT = 3000;
+const CONFIG = require('./app/config/configuracion')
+const app = require('./app/app')
 
-app.listen(PORT,()=>{
+app.listen(CONFIG.PORT,()=>{
     console.log('Aplicacion corriendo en puerto $ {PORT}');
 })
